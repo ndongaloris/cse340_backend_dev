e0954231c9d5a27d9ae66fc881adc81f6b012eed
ndongaloris <ndongajared@gmail.com>
2024-05-06 17:10:37 +0200

    Robust Error Handling

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const utilities = require("./utilities/index")
+const utilities = require("./utilities/")
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -32,7 +32,7 @@ app.use(static);
 // app.get("/", function(req, res){
 //   res.render("index", {title: "Home"})
 // }); --old
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute)
 // File Not Found Route - must be last route in list
@@ -48,9 +48,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
