ac67104d767495138ae3689fae667da35471481b
ndongaloris <ndongajared@gmail.com>
2024-05-06 16:52:28 +0200

    Error Handling

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const utilities = require("./utilities/index")
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -23,8 +24,6 @@ app.set("layout", "./layouts/layout");
  * Routes
  *************************/
 app.use(static);
-// Inventory routes
-app.use("/inv", inventoryRoute)
 /* ***********************
  * Index Route
  * req: incoming response traffic from the browser
@@ -32,8 +31,29 @@ app.use("/inv", inventoryRoute)
  *************************/
 // app.get("/", function(req, res){
 //   res.render("index", {title: "Home"})
-// });
+// }); --old
 app.get("/", baseController.buildHome);
+// Inventory routes
+app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+
+app.use(async (req, res, next) => {
+  next({status: 404, message: "404 - you really are good at getting lost, aren't you?"})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
