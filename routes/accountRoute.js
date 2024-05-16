736b911924179997f1edd13b7949bbbf35160304
ndongaloris <ndongajared@gmail.com>
2024-05-16 20:46:04 +0200

    Account: Process Registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to handle requests for building registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+router.post("/register",  utilities.handleErrors(accountController.registerAccount));
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const baseController = require("./controllers/baseController");
 const session = require("express-session");
 const pool = require('./database/');
 const accountRoute = require("./routes/accountRoute");
+const bodyParser = require("body-parser")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -43,6 +44,8 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res);
   next();
 })
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * Routes
